483e4f25114a32f40f3bb2437eb0b35624b25393
Mike Tonks <michael.tonks@form3.tech>
2023-06-16 17:40:03 +0100

    chore: adding generated files
    Signed-off-by: Mike Tonks <michael.tonks@form3.tech>

--- pkg/selector/aws/selector_test.go
+++ pkg/selector/aws/selector_test.go
@@ -1,3 +1,18 @@
+// Copyright 2021 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package aws_test
 
 import (
@@ -13,6 +28,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// StubClient implements the interface
 type StubClient struct {
 	Input  *ec2.DescribeInstancesInput
 	Output *ec2.DescribeInstancesOutput
