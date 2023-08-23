7f54bbe2df99a38a9a6c1c0dc2847c70622eb6c8
Kenneth Buck <kenneth.buck@form3.tech>
2023-08-23 15:58:07 -0300

    fix: improve zap logging configuration
    Configure structured logging and conventional field names for timestamp,
    level, etc.
    
    Truncate log fields over a certain limit.
    
    Allow configuration of key logging parameters through environment.
    
    Co-authored-by: Kenneth Buck <kenneth.buck@form3.tech>

--- pkg/log/zap.go
+++ pkg/log/zap.go
@@ -16,7 +16,10 @@
 package log
 
 import (
+	"encoding/json"
 	"io"
+	"os"
+	"strconv"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zapr"
@@ -24,16 +27,113 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+const LogFormat = "LOG_FORMAT"
+const LogKeyMessage = "LOG_KEY_MESSAGE"
+const LogKeyTimestamp = "LOG_KEY_TIMESTAMP"
+const LogMaxFieldSize = "LOG_MAX_FIELD_SIZE"
+const LogTimestampFormat = "LOG_TIMESTAMP_FORMAT"
+
 // NewDefaultZapLogger is the recommended way to create a new logger, you could call this function to initialize the root
 // logger of your application, and provide it to your components, by fx or manually.
 func NewDefaultZapLogger() (logr.Logger, error) {
-	// change the configuration in the future if needed.
-	zapLogger, err := zap.NewDevelopment()
+	envLevel := os.Getenv("LOG_LEVEL")
+	logLevel := zap.InfoLevel
+	if envLevel != "" {
+		var err error
+		logLevel, err = zapcore.ParseLevel(envLevel)
+		if err != nil {
+			return logr.Discard(), err
+		}
+	}
+
+	envFormat := os.Getenv(LogFormat)
+	logFormat := "console"
+	if envFormat == "json" {
+		logFormat = "json"
+	}
+
+	var config zap.Config
+
+	if logFormat == "json" {
+		encoderConfig := zap.NewProductionEncoderConfig()
+
+		if v := os.Getenv(LogKeyMessage); v != "" {
+			encoderConfig.MessageKey = v
+		}
+		if v := os.Getenv(LogKeyTimestamp); v != "" {
+			encoderConfig.TimeKey = v
+		}
+
+		if v := os.Getenv(LogTimestampFormat); v != "" {
+			if v == "rfc3339" {
+				encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+			} else if v == "rfc3339nano" {
+				encoderConfig.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+			} else if v == "epoch" {
+				encoderConfig.EncodeTime = zapcore.EpochTimeEncoder
+			}
+		}
+
+		// If configured, truncate the fields to the configured size. This allows for reasonable configuration to prevent extremely
+		// long logging lines (e.g. by logging an object with a large collection of events attached), which can cause issues with
+		// log ingestion/collection, while guaranteeing that the output remains valid JSON.
+		envMaxFieldSize := os.Getenv(LogMaxFieldSize)
+		if envMaxFieldSize != "" {
+			maxFieldSize, err := strconv.Atoi(envMaxFieldSize)
+			if err == nil {
+				encoderConfig.NewReflectedEncoder = func(w io.Writer) zapcore.ReflectedEncoder {
+					enc := json.NewEncoder(newTruncatingWriter(w, maxFieldSize))
+					enc.SetEscapeHTML(false)
+					return enc
+				}
+			}
+		}
+
+		config = zap.NewProductionConfig()
+		config.EncoderConfig = encoderConfig
+	} else {
+		config = zap.NewDevelopmentConfig()
+	}
+
+	zapLogger, err := config.Build(zap.IncreaseLevel(logLevel))
 	if err != nil {
 		return logr.Discard(), err
 	}
+
 	logger := zapr.NewLogger(zapLogger)
 	return logger, nil
+}
+
+type truncatingWriter struct {
+	writer    io.Writer
+	enc       *json.Encoder
+	maxLength int
+}
+
+func newTruncatingWriter(writer io.Writer, maxLength int) truncatingWriter {
+	enc := json.NewEncoder(writer)
+	enc.SetEscapeHTML(false)
+
+	return truncatingWriter{
+		writer:    writer,
+		maxLength: maxLength,
+		enc:       enc,
+	}
+}
+
+func (tr truncatingWriter) Write(bytes []byte) (int, error) {
+	output := bytes
+
+	if len(bytes) > tr.maxLength {
+		output = append([]byte("TRUNCATED "), bytes[:tr.maxLength]...)
+	}
+
+	// Encode the (possibly truncated) JSON bytes as a JSON string to included in the JSON log entry.
+	// This prevents arbitrary JSON objects from being supplied as field values, which can cause problems for ingestion in some
+	// log aggregation platforms.
+	// E.g. k8s objects may include objects with `".":{}` in `FieldsV1` metadata can cause index failure where a key of '.' is treated
+	// specially.
+	return 0, tr.enc.Encode(string(output))
 }
 
 // NewZapLoggerWithWriter creates a new logger with io.writer
