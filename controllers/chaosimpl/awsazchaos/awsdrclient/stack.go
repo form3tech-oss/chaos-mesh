34e1aa71c0d533ce9cbfd441611a08c2b4318010
Kenneth Buck <kenneth.buck@form3.tech>
2023-08-28 12:20:30 -0300

    Add missing license headers

--- controllers/chaosimpl/awsazchaos/awsdrclient/stack.go
+++ controllers/chaosimpl/awsazchaos/awsdrclient/stack.go
@@ -1,3 +1,18 @@
+// Copyright 2021 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package awsdrclient
 
 import (
@@ -9,8 +24,9 @@ import (
 	autoscalingTypes "github.com/aws/aws-sdk-go-v2/service/autoscaling/types"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/chaos-mesh/chaos-mesh/controllers/chaosimpl/awsazchaos/ctxutil"
 	"github.com/go-logr/logr"
+
+	"github.com/chaos-mesh/chaos-mesh/controllers/chaosimpl/awsazchaos/ctxutil"
 )
 
 func NewPtr[T any](val T) *T {
