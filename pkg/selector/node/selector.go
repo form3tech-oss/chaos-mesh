71ae3675d18551eb7acfdc518fcc7f747f4d990e
Andrius Navasaitis <andrius.navasaitis@form3.tech>
2023-11-01 16:52:15 +0000

    wip: node selector mode

--- pkg/selector/node/selector.go
+++ pkg/selector/node/selector.go
@@ -21,6 +21,7 @@ import (
 
 	"go.uber.org/fx"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -28,12 +29,13 @@ import (
 	"github.com/go-logr/logr"
 
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
+	"github.com/chaos-mesh/chaos-mesh/pkg/selector/generic"
 )
 
 // const nodeSelectorName = "node"
 
 type SelectImpl struct {
-	c      client.Client
+	r      client.Reader
 	logger logr.Logger
 }
 
@@ -47,14 +49,16 @@ func (n *Node) Id() string {
 
 func (impl *SelectImpl) Select(ctx context.Context, nodeSelector *v1alpha1.NodeSelector) ([]*Node, error) {
 	if nodeSelector == nil {
-		return []*Node{}, nil
+		return nil, nil
 	}
 
+	selector := nodeSelector.Selector
+
 	var nodes []*Node
-	if len(nodeSelector.Selector.Names) > 0 {
-		for _, name := range nodeSelector.Selector.Names {
+	if len(selector.Names) > 0 {
+		for _, name := range selector.Names {
 			var node v1.Node
-			if err := impl.c.Get(ctx, types.NamespacedName{Name: name}, &node); err != nil {
+			if err := impl.r.Get(ctx, types.NamespacedName{Name: name}, &node); err != nil {
 				return nil, err
 			}
 			nodes = append(nodes, &Node{node.Name})
@@ -62,15 +66,15 @@ func (impl *SelectImpl) Select(ctx context.Context, nodeSelector *v1alpha1.NodeS
 	}
 
 	opts := &client.ListOptions{
-		LabelSelector: labels.SelectorFromSet(nodeSelector.Selector.LabelSelectors),
-		// FieldSelector: fields.SelectorFromSet(nodeSelector.Selector.FieldSelectors),
+		LabelSelector: labels.SelectorFromSet(selector.LabelSelectors),
+		FieldSelector: fields.SelectorFromSet(selector.FieldSelectors),
 	}
 
 	var nodeList v1.NodeList
 
-	if len(nodeSelector.Selector.LabelSelectors) > 0 || len(nodeSelector.Selector.FieldSelectors) > 0 {
-		if err := impl.c.List(ctx, &nodeList, opts); err != nil {
-			return []*Node{}, fmt.Errorf("listing nodes: %w", err)
+	if len(selector.LabelSelectors) > 0 || len(selector.FieldSelectors) > 0 {
+		if err := impl.r.List(ctx, &nodeList, opts); err != nil {
+			return nil, fmt.Errorf("listing nodes: %w", err)
 		}
 
 		for _, node := range nodeList.Items {
@@ -78,55 +82,39 @@ func (impl *SelectImpl) Select(ctx context.Context, nodeSelector *v1alpha1.NodeS
 		}
 	}
 
+	nodes, err := filterNodesByMode(nodes, nodeSelector.Mode, nodeSelector.Value)
+	if err != nil {
+		return nil, fmt.Errorf("filtering nodes by mode %q and value %q: %w", nodeSelector.Mode, nodeSelector.Value, err)
+	}
+
 	return nodes, nil
 }
 
-// func (s *SelectImpl) Match(obj client.Object) bool {
-// 	node := obj.(*v1.Node)
-// 	for _, n := range s.nodes {
-// 		if n.Name == node.Name {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
+func filterNodesByMode(nodes []*Node, mode v1alpha1.SelectorMode, value string) ([]*Node, error) {
+	indexes, err := generic.FilterObjectsByMode(mode, value, len(nodes))
+	if err != nil {
+		return nil, err
+	}
 
-// // if both setting Nodes and NodeSelectors, the node list will be combined.
-// func newNodeSelector(ctx context.Context, c client.Client, spec v1alpha1.PodSelectorSpec) (generic.Selector, error) {
-// 	if len(spec.Nodes) == 0 && len(spec.NodeSelectors) == 0 {
-// 		return &SelectImpl{nodes: []v1.Node{}}, nil
-// 	}
-// 	var nodes []v1.Node
-// 	if len(spec.Nodes) > 0 {
-// 		for _, name := range spec.Nodes {
-// 			var node v1.Node
-// 			if err := c.Get(ctx, types.NamespacedName{Name: name}, &node); err != nil {
-// 				return nil, err
-// 			}
-// 			nodes = append(nodes, node)
-// 		}
-// 	}
-// 	if len(spec.NodeSelectors) > 0 {
-// 		var nodeList v1.NodeList
-// 		if err := c.List(ctx, &nodeList, &client.ListOptions{
-// 			LabelSelector: labels.SelectorFromSet(spec.NodeSelectors),
-// 		}); err != nil {
-// 			return nil, err
-// 		}
-// 		nodes = append(nodes, nodeList.Items...)
-// 	}
-// 	return &SelectImpl{nodes: nodes}, nil
-// }
+	filteredNodes := make([]*Node, 0, len(nodes))
+
+	for _, index := range indexes {
+		index := index
+		filteredNodes = append(filteredNodes, nodes[index])
+	}
+	return filteredNodes, nil
+}
 
 type Params struct {
 	fx.In
 
-	Client client.Client
+	// Client client.Client
+	Reader client.Reader `name:"no-cache"`
 }
 
 func New(params Params, logger logr.Logger) *SelectImpl {
 	return &SelectImpl{
-		c:      params.Client,
+		r:      params.Reader,
 		logger: logger.WithName("node-selector"),
 	}
 }
