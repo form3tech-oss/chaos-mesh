e674c536961f1b32d0228e4010673f6183ec7771
Sergii Shutovskyi <sergii.shutovskyi@form3.tech>
2024-08-22 12:33:13 +0200

    feat: add retry to all api calls

--- controllers/chaosimpl/cloudstackhost/hoststop/impl.go
+++ controllers/chaosimpl/cloudstackhost/hoststop/impl.go
@@ -18,16 +18,17 @@ package hoststop
 import (
 	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"math/rand"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/apache/cloudstack-go/v2/cloudstack"
+	"github.com/avast/retry-go/v4"
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/chaos-mesh/chaos-mesh/api/v1alpha1"
@@ -46,13 +47,10 @@ const (
 	StateUp      = "Up"
 	StateRunning = "Running"
 	StateStopped = "Stopped"
-
-	UpCheckInterval = 30 * time.Second
-	UpCheckTimeout  = 10 * time.Minute
-
-	DownCheckInterval = 30 * time.Second
-	DownCheckTimeout  = 2 * time.Minute
 )
+
+var retryOpts = []retry.Option{retry.Attempts(10), retry.MaxDelay(5 * time.Second), retry.DelayType(retry.FixedDelay), retry.LastErrorOnly(true)}
+var waitRetryOpts = []retry.Option{retry.Attempts(20), retry.MaxDelay(30 * time.Second), retry.DelayType(retry.FixedDelay), retry.LastErrorOnly(true)}
 
 func (impl *Impl) Apply(ctx context.Context, index int, records []*v1alpha1.Record, obj v1alpha1.InnerObject) (v1alpha1.Phase, error) {
 	cloudstackchaos := obj.(*v1alpha1.CloudStackHostChaos)
@@ -60,21 +58,24 @@ func (impl *Impl) Apply(ctx context.Context, index int, records []*v1alpha1.Reco
 
 	client, err := utils.GetCloudStackClient(ctx, impl.Client, cloudstackchaos)
 	if err != nil {
-		return v1alpha1.NotInjected, fmt.Errorf("creating cloudstack api client: %w", err)
+		return v1alpha1.NotInjected, errors.Wrap(err, "creating cloudstack api client")
 	}
 
 	var selector v1alpha1.CloudStackHostChaosSelector
 	if err := json.Unmarshal([]byte(records[index].Id), &selector); err != nil {
-		return v1alpha1.NotInjected, fmt.Errorf("decoding selector: %w", err)
+		return v1alpha1.NotInjected, errors.Wrapf(err, "decoding selector: %s", records[index].Id)
 	}
 
 	params := utils.SelectorToListParams(&selector)
 	params.SetOutofbandmanagementenabled(true)
 	params.SetOutofbandmanagementpowerstate("On")
 
-	resp, err := client.Host.ListHosts(params)
+	resp, err := retry.DoWithData(func() (*cloudstack.ListHostsResponse, error) {
+		return client.Host.ListHosts(params)
+	})
 	if err != nil {
-		return v1alpha1.NotInjected, fmt.Errorf("listing hosts: %w", err)
+		impl.Log.Error(err, "Failed to list matching hosts", "selector", records[index].Id)
+		return v1alpha1.NotInjected, errors.Wrap(err, "listing hosts")
 	}
 
 	if len(resp.Hosts) == 0 {
@@ -87,61 +88,69 @@ func (impl *Impl) Apply(ctx context.Context, index int, records []*v1alpha1.Reco
 	impl.Log.Info("Stopping host", "id", h.Id, "name", h.Name, "dry-run", spec.DryRun)
 
 	if !spec.DryRun {
-		_, err := client.OutofbandManagement.IssueOutOfBandManagementPowerAction(client.OutofbandManagement.NewIssueOutOfBandManagementPowerActionParams(ActionOff, h.Id))
-		if err != nil {
-			return v1alpha1.NotInjected, fmt.Errorf("stopping host %s: %w", h.Name, err)
+		params := client.OutofbandManagement.NewIssueOutOfBandManagementPowerActionParams(ActionOff, h.Id)
+		if err := retry.Do(func() error {
+			_, err := client.OutofbandManagement.IssueOutOfBandManagementPowerAction(params)
+			return err
+		}, retryOpts...); err != nil {
+			impl.Log.Error(err, "Failed to stop the host", "name", h.Name)
+			return v1alpha1.NotInjected, errors.Wrapf(err, "stopping host %s", h.Name)
 		}
-	}
 
-	impl.Log.Info("Stopped host", "id", h.Id, "name", h.Name, "dry-run", spec.DryRun)
+		impl.Log.Info("Stopped host", "id", h.Id, "name", h.Name)
+	}
 
 	return v1alpha1.Injected, nil
 }
 
 func (impl *Impl) Recover(ctx context.Context, index int, records []*v1alpha1.Record, obj v1alpha1.InnerObject) (v1alpha1.Phase, error) {
 	impl.Log.Info("Starting hypervisor recovery")
+
 	cloudstackchaos := obj.(*v1alpha1.CloudStackHostChaos)
 	spec := cloudstackchaos.Spec
 
 	client, err := utils.GetCloudStackClient(ctx, impl.Client, cloudstackchaos)
 	if err != nil {
-		return v1alpha1.Injected, fmt.Errorf("creating cloudstack api client: %w", err)
+		return v1alpha1.Injected, errors.Wrap(err, "creating cloudstack api client")
 	}
-	impl.Log.Info("Parsing selector")
 
 	var selector v1alpha1.CloudStackHostChaosSelector
 	if err := json.Unmarshal([]byte(records[index].Id), &selector); err != nil {
-		return v1alpha1.Injected, fmt.Errorf("decoding selector: %w", err)
+		return v1alpha1.Injected, errors.Wrapf(err, "decoding selector: %s", records[index].Id)
 	}
-
-	impl.Log.Info("Looking for hosts to recover", "selector", records[index].Id)
 
 	params := utils.SelectorToListParams(&selector)
 	params.SetOutofbandmanagementenabled(true)
 	params.SetOutofbandmanagementpowerstate("Off")
 
-	resp, err := client.Host.ListHosts(params)
+	resp, err := retry.DoWithData(func() (*cloudstack.ListHostsResponse, error) {
+		return client.Host.ListHosts(params)
+	}, retryOpts...)
 	if err != nil {
-		return v1alpha1.Injected, fmt.Errorf("listing hosts: %w", err)
+		impl.Log.Error(err, "Failed to list offline hosts", "selector", records[index].Id)
+		return v1alpha1.Injected, errors.Wrap(err, "listing hosts")
 	}
-
-	impl.Log.Info("Found hosts to start", "len", len(resp.Hosts))
 
 	for _, h := range resp.Hosts {
 		impl.Log.Info("Starting host", "id", h.Id, "name", h.Name, "dry-run", spec.DryRun)
 
-		if !spec.DryRun {
-			_, err := client.OutofbandManagement.IssueOutOfBandManagementPowerAction(client.OutofbandManagement.NewIssueOutOfBandManagementPowerActionParams(ActionOn, h.Id))
-			if err != nil {
-				return v1alpha1.Injected, fmt.Errorf("starting host %s: %w", h.Name, err)
-			}
-
-			if err := waitForHostToBeUp(client, h.Id); err != nil {
-				return v1alpha1.Injected, err
-			}
-
-			impl.Log.Info("Started host", "id", h.Id, "name", h.Name, "dry-run", spec.DryRun)
+		if spec.DryRun {
+			continue
 		}
+		if err := retry.Do(func() error {
+			_, err := client.OutofbandManagement.IssueOutOfBandManagementPowerAction(client.OutofbandManagement.NewIssueOutOfBandManagementPowerActionParams(ActionOn, h.Id))
+			return err
+		}, retryOpts...); err != nil {
+			impl.Log.Error(err, "Failed to start host", "host", h.Name)
+			return v1alpha1.Injected, errors.Wrapf(err, "starting host %s", h.Name)
+		}
+
+		if err := waitForHostToBeUp(client, h.Id); err != nil {
+			impl.Log.Error(err, "Host failed to start", "host", h.Name)
+			return v1alpha1.Injected, err
+		}
+
+		impl.Log.Info("Started host", "id", h.Id, "name", h.Name)
 	}
 
 	if err := impl.startVMs(client, spec.DryRun); err != nil {
@@ -165,85 +174,61 @@ func NewImpl(c client.Client, log logr.Logger) *Impl {
 }
 
 func waitForVmToBeRunning(client *cloudstack.CloudStackClient, vmId string) error {
-	ticker := time.NewTicker(UpCheckInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			vm, _, err := client.VirtualMachine.GetVirtualMachineByID(vmId)
-			if err != nil {
-				return fmt.Errorf("failed to query status for vm %s: %w", vmId, err)
-			}
-			if vm.State == StateRunning {
-				return nil
-			}
-		case <-time.After(UpCheckTimeout):
-			return fmt.Errorf("timed out waiting for vm %s to be up", vmId)
+	return retry.Do(func() error {
+		vm, _, err := client.VirtualMachine.GetVirtualMachineByID(vmId)
+		if err != nil {
+			return errors.Wrapf(err, "failed to query status for vm %s", vmId)
 		}
-	}
+		if vm.State == StateRunning {
+			return nil
+		}
+
+		return errors.Errorf("VM %s is not running", vmId)
+	}, waitRetryOpts...)
 }
 func waitForHostToBeUp(client *cloudstack.CloudStackClient, hostId string) error {
-	ticker := time.NewTicker(UpCheckInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			host, _, err := client.Host.GetHostByID(hostId)
-			if err != nil {
-				return fmt.Errorf("failed to query status for host %s: %w", hostId, err)
-			}
-			if host.State == StateUp {
-				return nil
-			}
-		case <-time.After(UpCheckTimeout):
-			return fmt.Errorf("timed out waiting for host %s to be up", hostId)
+	return retry.Do(func() error {
+		host, _, err := client.Host.GetHostByID(hostId)
+		if err != nil {
+			return errors.Wrapf(err, "failed to query status for host %s", hostId)
 		}
-	}
+		if host.State == StateUp {
+			return nil
+		}
+		return errors.Errorf("host %s is not up", hostId)
+	}, waitRetryOpts...)
 }
 func (impl *Impl) getK8sNodesWhenReady(ctx context.Context) ([]v1.Node, error) {
-	ticker := time.NewTicker(UpCheckInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			nodes := v1.NodeList{}
-			err := impl.List(ctx, &nodes)
-			if err != nil {
-				impl.Log.Error(err, "failed to list nodes")
-				continue
-			}
-			unreadyNodes := []string{}
-			for _, node := range nodes.Items {
-				for _, condition := range node.Status.Conditions {
-					if condition.Type == v1.NodeReady && condition.Status != v1.ConditionTrue {
-						unreadyNodes = append(unreadyNodes, node.Name)
-						break
-					}
+	return retry.DoWithData(func() ([]v1.Node, error) {
+		nodeList := v1.NodeList{}
+		err := impl.List(ctx, &nodeList)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to list nodes")
+		}
+		unreadyNodes := []string{}
+		for _, node := range nodeList.Items {
+			for _, condition := range node.Status.Conditions {
+				if condition.Type == v1.NodeReady && condition.Status != v1.ConditionTrue {
+					unreadyNodes = append(unreadyNodes, node.Name)
+					break
 				}
 			}
-			if len(unreadyNodes) > 0 {
-				impl.Log.Info("nodes not ready", "nodes", strings.Join(unreadyNodes, ", "))
-			} else {
-				return nodes.Items, nil
-			}
-
-		case <-time.After(UpCheckTimeout):
-			return nil, errors.New("timed out waiting for nodes to be ready")
 		}
-	}
+		if len(unreadyNodes) > 0 {
+			return nil, errors.Errorf("nodes %s not ready", strings.Join(unreadyNodes, ", "))
+		}
+		return nodeList.Items, nil
+
+	}, waitRetryOpts...)
 }
 
 func (impl *Impl) uncordonK8sNodes(ctx context.Context, dryRun bool) error {
 	impl.Log.Info("Will uncordon ready nodes")
 	nodes, err := impl.getK8sNodesWhenReady(ctx)
 	if err != nil {
-		impl.Log.Error(err, "nodes not ready")
+		impl.Log.Error(err, "Nodes not ready")
 		return nil
 	}
-	impl.Log.Info("Found ready nodes", "len", len(nodes))
 
 	for _, node := range nodes {
 		isApplicable := false
@@ -256,16 +241,31 @@ func (impl *Impl) uncordonK8sNodes(ctx context.Context, dryRun bool) error {
 			continue
 		}
 
-		impl.Log.Info("Uncordon node", "node", node.Name, "dryRun", dryRun)
+		impl.Log.Info("Uncordon unschedulable node", "node", node.Name, "dryRun", dryRun)
 		if dryRun {
 			continue
 		}
-		node.Spec.Unschedulable = false
-		err := impl.Update(ctx, &node)
+
+		err := retry.Do(func() error {
+			nodeItem := v1.Node{}
+			if err := impl.Get(ctx, types.NamespacedName{
+				Namespace: node.Namespace,
+				Name:      node.Name,
+			}, &nodeItem); err != nil {
+				return retry.Unrecoverable(errors.Wrapf(err, "could not find node %s", node.Name))
+			}
+			if !nodeItem.Spec.Unschedulable {
+				return nil // nothing to do
+			}
+			nodeItem.Spec.Unschedulable = false
+			return impl.Update(ctx, &nodeItem)
+		}, retryOpts...)
+
 		if err != nil {
-			impl.Log.Error(err, "failed to uncordon node", "node", node.Name)
+			impl.Log.Error(err, "Failed to uncordon node", "node", node.Name)
+		} else {
+			impl.Log.Info("Uncordoned node", "node", node.Name)
 		}
-		impl.Log.Info("Uncordoned node", "node", node.Name)
 	}
 	return nil
 }
@@ -275,11 +275,12 @@ func (impl *Impl) startVMs(client *cloudstack.CloudStackClient, dryRun bool) err
 	params := client.VirtualMachine.NewListVirtualMachinesParams()
 	params.SetState(StateStopped)
 
-	resp, err := client.VirtualMachine.ListVirtualMachines(params)
+	resp, err := retry.DoWithData(func() (*cloudstack.ListVirtualMachinesResponse, error) {
+		return client.VirtualMachine.ListVirtualMachines(params)
+	}, retryOpts...)
 	if err != nil {
 		return err
 	}
-	impl.Log.Info("Found vms to start", "len", len(resp.VirtualMachines))
 
 	wg := sync.WaitGroup{}
 	for _, vm := range resp.VirtualMachines {
@@ -295,15 +296,18 @@ func (impl *Impl) startVMs(client *cloudstack.CloudStackClient, dryRun bool) err
 			startParams := client.VirtualMachine.NewStartVirtualMachineParams(vm.Id)
 			startParams.SetConsiderlasthost(true) // try to schedule to the same host
 
-			_, err = client.VirtualMachine.StartVirtualMachine(startParams)
-			if err != nil {
+			if retry.Do(func() error {
+				_, err := client.VirtualMachine.StartVirtualMachine(startParams)
+				return err
+			}, retryOpts...); err != nil {
 				impl.Log.Error(err, "failed to start stopped vm", "name", vm.Name)
 			}
-			err := waitForVmToBeRunning(client, vm.Id)
-			if err != nil {
+
+			if err := waitForVmToBeRunning(client, vm.Id); err != nil {
 				impl.Log.Error(err, "failed to wait for vm to be running", "name", vm.Name)
+			} else {
+				impl.Log.Info("Started VM", "id", vm.Id, "name", vm.Name)
 			}
-			impl.Log.Info("Started VM", "id", vm.Id, "name", vm.Name)
 
 		}(vm)
 	}
@@ -316,22 +320,32 @@ func (impl *Impl) destroyStuckSystemVMs(client *cloudstack.CloudStackClient, dry
 	impl.Log.Info("Will destroy stuck system VMs")
 	params := client.SystemVM.NewListSystemVmsParams()
 	params.SetState(StateStopped)
-	resp, err := client.SystemVM.ListSystemVms(params)
+
+	resp, err := retry.DoWithData(func() (*cloudstack.ListSystemVmsResponse, error) {
+		return client.SystemVM.ListSystemVms(params)
+	})
 	if err != nil {
 		return err
 	}
-	impl.Log.Info("Found system vms to start", "len", len(resp.SystemVms))
 
 	for _, vm := range resp.SystemVms {
 		impl.Log.Info("Destroying system VM", "id", vm.Id, "name", vm.Name, "dryRun", dryRun)
 		if dryRun {
 			continue
 		}
-		_, err := client.SystemVM.DestroySystemVm(client.SystemVM.NewDestroySystemVmParams(vm.Id))
+
+		params := client.SystemVM.NewDestroySystemVmParams(vm.Id)
+		err := retry.Do(func() error {
+			_, err := client.SystemVM.DestroySystemVm(params)
+			return err
+		}, retryOpts...)
+
 		if err != nil {
-			return fmt.Errorf("failed to destroy system vm %s: %w", vm.Id, err)
+			impl.Log.Error(err, "Failed to destroy system vm", "id", vm.Id, "name", vm.Name)
+		} else {
+			impl.Log.Info("Destroyed system VM", "id", vm.Id, "name", vm.Name)
 		}
-		impl.Log.Info("Destroyed system VM", "id", vm.Id, "name", vm.Name)
 	}
+
 	return nil
 }
